cbbecda875b8604ec872d4da8e7581a6d00701e1
huliang <huliang0222@gmail.com>
2024-08-14 16:38:58 +0800

    chore: env url

--- src/service/stake/index.ts
+++ src/service/stake/index.ts
@@ -1,34 +1,36 @@
+import config from '@/config';
 import axios from 'axios';
 
 export async function fetchPools(body) {
   // const { data } = await axios.post(`${config.stakeUrl}/api/v1/pools`, body);
   // const { data } = await axios.post(`/stake/api/v1/pools`, body);
-  const { data } = await axios.post(`https://apit.vikingrunes.io/staking/api/v1/pools`, body);
+  const { data } = await axios.post(`${config.stakeUrl}/api/v1/pools`, body);
+
   if (data.code !== 200) throw Error(data.message);
   return data.data;
 }
 
 export async function fetchPoolDetail(id) {
   // const { data } = await axios.post(`${config.stakeUrl}/stake/api/v1/pool/${id}`);
-  const { data } = await axios.post(`/stake/api/v1/pool/${id}`);
+  const { data } = await axios.post(`${config.stakeUrl}/stake/api/v1/pool/${id}`);
   if (data.code !== 200) throw Error(data.message);
   return data.data;
 }
 
 export async function txStake(body) {
-  const { data } = await axios.post(`/stake/api/tx/stake`, body);
+  const { data } = await axios.post(`${config.stakeUrl}/stake/api/tx/stake`, body);
   if (data.code !== 200) throw Error(data.message);
   return data.data;
 }
 
 export async function fetchOrders(body) {
-  const { data } = await axios.post(`/stake/api/v1/orders`, body);
+  const { data } = await axios.post(`${config.stakeUrl}/stake/api/v1/orders`, body);
   if (data.code !== 200) throw Error(data.message);
   return data.data;
 }
 
 export async function fetchOrder(uuid) {
-  const { data } = await axios.post(`/stake/api/v1/order/${uuid}`, {});
+  const { data } = await axios.post(`${config.stakeUrl}/stake/api/v1/order/${uuid}`, {});
   if (data.code !== 200) throw Error(data.message);
   return data.data;
 }
