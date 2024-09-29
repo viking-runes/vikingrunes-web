21fce7ff0d87f3ec50eaf5b2f083b21ad660ed05
huliang <huliang0222@gmail.com>
2024-09-29 09:46:38 +0800

    fix: page

--- src/pages/nft/index.tsx
+++ src/pages/nft/index.tsx
@@ -42,6 +42,12 @@ const NftDetail = () => {
     fetchMintEnv().then((res) => {
       setMintEnv(res);
     });
+
+    setInterval(() => {
+      fetchMintEnv().then((res) => {
+        setMintEnv(res);
+      });
+    }, 10000);
   }, []);
 
   const onSubmitMint = async () => {
@@ -99,10 +105,10 @@ const NftDetail = () => {
             {/* <iframe sandbox="allow-scripts" loading="lazy" src="https://ord.opendao.dev/preview/7f9f456016ca5c500c683efa2572718a43e35a91184d1d28b09feb898199d941i0" height={400} width={400}></iframe> */}
             {/* <img src="/assets/mockImage.png" alt="" width={400} height={400} /> */}
           </Box>
-          <Stack sx={{ width: 282, height: 30, background: '#242738', borderRadius: 2.5, justifyContent: 'center', textAlign: 'center', fontSize: 14, mt: 1.5, mb: 5 }}>You already mint: 500</Stack>
+          {/* <Stack sx={{ width: 282, height: 30, background: '#242738', borderRadius: 2.5, justifyContent: 'center', textAlign: 'center', fontSize: 14, mt: 1.5, mb: 5 }}>You already mint: 500</Stack> */}
           <Stack sx={{ border: '1px solid #242738', borderRadius: 1.25, width: 400, pb: 5 }}>
             <Stack sx={{ height: 64, background: '#1A1C28', justifyContent: 'center', textAlign: 'center', fontSize: 18, color: '#EBB94C' }}>Salute to all the warriors</Stack>
-            <Stack sx={{ textAlign: 'justify', px: 1, py: 2 }} spacing={2}>
+            <Stack sx={{ px: 1, py: 2 }} spacing={2}>
               <Typography fontSize={12}>Vikings consisted of explorers, berserkers, merchants, sailors, pirates, wizards and craftsmen, and were good at sailing.</Typography>
               <Typography fontSize={12}>Legend has it that Vikings wore horned helmets to fight and were extremely brave. From the 8th to the 11th century AD, they conquered most of the European coast.</Typography>
               <Typography fontSize={12}>We have specially created the Ordinals collection &quot;Viking Warrior&ldquo; to pay tribute to the warriors who sailed on bitcoin.</Typography>
@@ -114,16 +120,16 @@ const NftDetail = () => {
         {/* {mintEnv && ( */}
         <Stack width={'100%'}>
           <Typography sx={{ fontSize: 18, color: '#EBB94C', pb: 2 }}>Viking Warrior</Typography>
-          <Typography sx={{ fontSize: 14, pb: 3.75 }}>Quantity: {mintEnv?.supply - mintEnv?.minted} &nbsp;&nbsp; Price: Freemint</Typography>
+          {/* <Typography sx={{ fontSize: 14, pb: 3.75 }}>Quantity: {mintEnv?.supply - mintEnv?.minted} &nbsp;&nbsp; Price: Freemint</Typography> */}
 
           {/* free box */}
           <Stack sx={{ border: '1px solid #363944', px: 1.5, py: 1.25, borderRadius: 1.25 }} spacing={1.25}>
             <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
               <Stack direction={'row'} alignItems={'center'} spacing={1}>
                 <Typography fontSize={12} px={1.75} py={0.5} sx={{ background: '#363944', borderRadius: 5 }}>
-                  Free
+                  Whitelist
                 </Typography>
-                <img src="/assets/lock.svg" alt="" width={16} height={16} />
+                {/* <img src="/assets/lock.svg" alt="" width={16} height={16} /> */}
               </Stack>
               {/* <Typography fontSize={14}>0.0014 BTC (140000 sats)</Typography> */}
             </Stack>
@@ -133,31 +139,36 @@ const NftDetail = () => {
                   Total supply
                 </Typography>
                 <Typography display={'inherit'}>{mintEnv?.supply}</Typography>
+
+                <Typography sx={{ fontSize: 14 }}> &nbsp;&nbsp; Price: Freemint</Typography>
               </Stack>
-              <Typography fontSize={14} color={'#363944'}>
-                ENDED
+              <Typography fontSize={20} color={'#363944'}>
+                {mintEnv?.minted >= mintEnv?.supply ? 'ENDED' : 'START'}
               </Typography>
             </Stack>
           </Stack>
 
           <Box py={2} display={'flex'} justifyContent={'flex-end'}>
-            <CountBox
-              key={'free'}
-              maxValue={mintEnv?.supply - mintEnv?.minted}
-              onChange={(value) => {
-                mintCount.current = value;
-              }}
-            />
+            {false && (
+              <CountBox
+                key={'free'}
+                maxValue={mintEnv?.supply - mintEnv?.minted}
+                onChange={(value) => {
+                  mintCount.current = value;
+                }}
+              />
+            )}
           </Box>
           <LinearProgress
             variant="determinate"
             value={(mintEnv?.minted / mintEnv?.supply) * 100}
+            // value={40}
             sx={{
               height: 12,
               borderRadius: 6,
-              backgroundColor: '#EBB94C',
+              backgroundColor: 'white',
               '.MuiLinearProgress-bar1Determinate': {
-                backgroundColor: 'white',
+                backgroundColor: '#EBB94C',
                 borderRadius: 6,
               },
             }}
